89ba3d65bc24b6dd1483f37f410d179787aac6cf
chalpenny <cp.halpenny@gmailcom>
2019-10-24 21:40:17 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra::
+    
+    configure do
+        enable :sessions
+        set :session_secret, "1234catmeow"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+       @purchase = params[:item] 
+    end
+
 end
